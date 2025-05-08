e4ad9b6ef66abb9f1558221f0c9acfdaec81906d
TannyJung <42003724+TannyJungMC@users.noreply.github.com>
2025-05-09 06:35:42 +0700

    Back to use completable future again

--- src/main/java/tannyjung/tanshugetrees_handcode/systems/config/ConfigRepairAll.java
+++ src/main/java/tannyjung/tanshugetrees_handcode/systems/config/ConfigRepairAll.java
@@ -6,23 +6,29 @@ import tannyjung.tanshugetrees_handcode.Handcode;
 import tannyjung.tanshugetrees_handcode.misc.FileManager;
 import tannyjung.tanshugetrees_handcode.misc.GameUtils;
 
+import java.util.concurrent.CompletableFuture;
+
 public class ConfigRepairAll {
 
 	public static void start (LevelAccessor level) {
 
-		GameUtils.sendChatMessage(level, "@a", "gray", "THT : Repaired The Config");
+		CompletableFuture.runAsync(() -> {
 
-		FileManager.createFolder(Handcode.directory_config + "/custom_packs/.organized");
-		FileManager.createFolder(Handcode.directory_config + "/generated");
+			FileManager.createFolder(Handcode.directory_config + "/custom_packs/.organized");
+			FileManager.createFolder(Handcode.directory_config + "/generated");
 
-		CustomPackOrganized.clearFolder();
-		CustomPackIncompatible.scanMain();
-		CustomPackOrganized.start();
-		CustomPackIncompatible.scanOrganized();
+			CustomPackOrganized.clearFolder();
+			CustomPackIncompatible.scanMain();
+			CustomPackOrganized.start();
+			CustomPackIncompatible.scanOrganized();
 
-		ConfigMain.repair();
-		ConfigAutoGen.repair();
-		ConfigPlacement.start();
+			ConfigMain.repair();
+			ConfigAutoGen.repair();
+			ConfigPlacement.start();
+
+			GameUtils.sendChatMessage(level, "@a", "gray", "THT : Repaired The Config");
+
+		});
 
 	}
 
