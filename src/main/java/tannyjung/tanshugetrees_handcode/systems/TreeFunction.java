4bfd33cc723a3cade4c0e0e8e64c1bef2fd753d2
TannyJung <42003724+TannyJungMC@users.noreply.github.com>
2025-05-27 03:53:59 +0700

    Protect some crash by array

--- src/main/java/tannyjung/tanshugetrees_handcode/systems/TreeFunction.java
+++ src/main/java/tannyjung/tanshugetrees_handcode/systems/TreeFunction.java
@@ -5,9 +5,11 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.features.FeatureUtils;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.chunk.ChunkStatus;
 import tannyjung.tanshugetrees.TanshugetreesMod;
 import tannyjung.tanshugetrees_handcode.Handcode;
 import tannyjung.tanshugetrees_handcode.misc.GameUtils;
@@ -19,89 +21,145 @@ import java.io.FileReader;
 
 public class TreeFunction {
 
-	public static void run (WorldGenLevel world_gen, String path, int posX, int posY, int posZ) {
+	public static void run (LevelAccessor level, String path, int posX, int posY, int posZ) {
 
 		File file = new File(Handcode.directory_config + "/custom_packs/.organized/functions/" + path);
 
 		if (file.exists() == true && file.isDirectory() == false) {
 
-			try { BufferedReader buffered_reader = new BufferedReader(new FileReader(file)); String read_all = ""; while ((read_all = buffered_reader.readLine()) != null) {
+			WorldGenLevel world_gen = (WorldGenLevel) level;
+			BlockPos pos = null;
 
-				{
+			String[] get = new String[0];
+			double chance = 0.0;
+			BlockState block = Blocks.AIR.defaultBlockState();
+			boolean keep = false;
 
-					if (read_all.equals("") == false) {
+			String[] offset_pos = new String[0];
+			int offset_posX = 0;
+			int offset_posY = 0;
+			int offset_posZ = 0;
+			String[] min_max = new String[0];
+			int minX = 0;
+			int minY = 0;
+			int minZ = 0;
+			int maxX = 0;
+			int maxY = 0;
+			int maxZ = 0;
+			int startX = 0;
+			int startY = 0;
+			int startZ = 0;
+			int endX = 0;
+			int endY = 0;
+			int endZ = 0;
 
-						if (read_all.startsWith("block = ") == true) {
+			String feature = "";
 
-							{
+			{
 
-								String[] get = read_all.replace("block = ", "").split(" \\| ");
+				try { BufferedReader buffered_reader = new BufferedReader(new FileReader(file)); String read_all = ""; while ((read_all = buffered_reader.readLine()) != null) {
 
-								double chance = Double.parseDouble(get[0]);
-								BlockState block = GameUtils.textToBlock(get[3]);
-								boolean keep = get[4].equals("true");
+					{
 
-								// Cancellation Conditions
+						if (read_all.equals("") == false) {
+
+							if (read_all.startsWith("block = ") == true) {
+
 								{
 
-									if (Math.random() >= chance) {
+									try {
 
-										continue;
+										get = read_all.replace("block = ", "").split(" \\| ");
+										chance = Double.parseDouble(get[0]);
+										block = GameUtils.textToBlock(get[3]);
+										keep = get[4].equals("true");
+
+									} catch (Exception e) {
+
+										return;
 
 									}
 
-									if (block == Blocks.AIR.defaultBlockState()) {
+									// Cancellation Conditions
+									{
 
-										continue;
+										if (Math.random() >= chance) {
+
+											continue;
+
+										}
+
+										if (block == Blocks.AIR.defaultBlockState()) {
+
+											continue;
+
+										}
 
 									}
 
-								}
+									// Get Pos
+									{
 
-								String[] offset_pos = get[1].split("/");
-								int offset_posX = Integer.parseInt(offset_pos[0]);
-								int offset_posY = Integer.parseInt(offset_pos[1]);
-								int offset_posZ = Integer.parseInt(offset_pos[2]);
+										try {
 
-								String[] min_max = get[2].split("/");
-								int minX = Integer.parseInt(min_max[0]);
-								int minY = Integer.parseInt(min_max[1]);
-								int minZ = Integer.parseInt(min_max[2]);
-								int maxX = Integer.parseInt(min_max[3]);
-								int maxY = Integer.parseInt(min_max[4]);
-								int maxZ = Integer.parseInt(min_max[5]);
+											offset_pos = get[1].split("/");
+											offset_posX = Integer.parseInt(offset_pos[0]);
+											offset_posY = Integer.parseInt(offset_pos[1]);
+											offset_posZ = Integer.parseInt(offset_pos[2]);
 
-								int startX = Math.min(0, Mth.nextInt(RandomSource.create(), minX, maxX));
-								int startY = Math.min(0, Mth.nextInt(RandomSource.create(), minY, maxY));
-								int startZ = Math.min(0, Mth.nextInt(RandomSource.create(), minZ, maxZ));
-								int endX = Math.max(0, Mth.nextInt(RandomSource.create(), minX, maxX));
-								int endY = Math.max(0, Mth.nextInt(RandomSource.create(), minY, maxY));
-								int endZ = Math.max(0, Mth.nextInt(RandomSource.create(), minZ, maxZ));
+											min_max = get[2].split("/");
+											minX = Integer.parseInt(min_max[0]);
+											minY = Integer.parseInt(min_max[1]);
+											minZ = Integer.parseInt(min_max[2]);
+											maxX = Integer.parseInt(min_max[3]);
+											maxY = Integer.parseInt(min_max[4]);
+											maxZ = Integer.parseInt(min_max[5]);
 
-								for (int testX = startX; testX <= endX; testX++) {
+											startX = Math.min(0, Mth.nextInt(RandomSource.create(), minX, maxX));
+											startY = Math.min(0, Mth.nextInt(RandomSource.create(), minY, maxY));
+											startZ = Math.min(0, Mth.nextInt(RandomSource.create(), minZ, maxZ));
+											endX = Math.max(0, Mth.nextInt(RandomSource.create(), minX, maxX));
+											endY = Math.max(0, Mth.nextInt(RandomSource.create(), minY, maxY));
+											endZ = Math.max(0, Mth.nextInt(RandomSource.create(), minZ, maxZ));
 
-									for (int testY = startY; testY <= endY; testY++) {
+										} catch (Exception e) {
 
-										for (int testZ = startZ; testZ <= endZ; testZ++) {
+											return;
 
-											BlockPos pos = new BlockPos(posX + offset_posX + testX, posY + offset_posY + testY, posZ + offset_posZ + testZ);
+										}
 
-											// Keep
-											{
+									}
 
-												if (keep == true) {
+									for (int testX = startX; testX <= endX; testX++) {
 
-													if (GameUtils.isBlockTaggedAs(world_gen.getBlockState(pos), "tanshugetrees:passable_blocks") == false || world_gen.isWaterAt(pos) == true) {
+										for (int testY = startY; testY <= endY; testY++) {
 
-														continue;
+											for (int testZ = startZ; testZ <= endZ; testZ++) {
+
+												pos = new BlockPos(posX + offset_posX + testX, posY + offset_posY + testY, posZ + offset_posZ + testZ);
+
+												// Keep
+												{
+
+													if (keep == true) {
+
+														if (GameUtils.isBlockTaggedAs(level.getBlockState(pos), "tanshugetrees:passable_blocks") == false || level.isWaterAt(pos) == true) {
+
+															continue;
+
+														}
 
 													}
 
 												}
 
-											}
+												if (level.hasChunk(pos.getX() >> 4, pos.getZ() >> 4) == true && level.getChunk(pos).getStatus().isOrAfter(ChunkStatus.SURFACE) == true) {
 
-											world_gen.setBlock(new BlockPos(pos), block, 2);
+													level.setBlock(new BlockPos(pos), block, 2);
+
+												}
+
+											}
 
 										}
 
@@ -109,39 +167,48 @@ public class TreeFunction {
 
 								}
 
-							}
+							} else if (read_all.startsWith("feature = ") == true) {
 
-						} else if (read_all.startsWith("feature = ") == true) {
+								{
 
-							{
+									try {
 
-								String[] get = read_all.replace("feature = ", "").split(" \\| ");
-								double chance = Double.parseDouble(get[0]);
-								String[] offset_pos = get[1].split("/");
-								int offset_posX = Integer.parseInt(offset_pos[0]);
-								int offset_posY = Integer.parseInt(offset_pos[1]);
-								int offset_posZ = Integer.parseInt(offset_pos[2]);
-								String feature = get[2];
+										get = read_all.replace("feature = ", "").split(" \\| ");
+										chance = Double.parseDouble(get[0]);
+										offset_pos = get[1].split("/");
+										offset_posX = Integer.parseInt(offset_pos[0]);
+										offset_posY = Integer.parseInt(offset_pos[1]);
+										offset_posZ = Integer.parseInt(offset_pos[2]);
+										feature = get[2];
 
-								if (Math.random() < chance) {
+									} catch (Exception e) {
 
-									world_gen.registryAccess().registryOrThrow(Registries.CONFIGURED_FEATURE).getHolderOrThrow(FeatureUtils.createKey(feature)).value().place((WorldGenLevel) world_gen, world_gen.getLevel().getChunkSource().getGenerator(), world_gen.getRandom(), BlockPos.containing(posX + offset_posX, posY + offset_posY, posZ + offset_posZ));
+										return;
+
+									}
+
+									if (Math.random() < chance) {
+
+										pos = new BlockPos(posX + offset_posX, posY + offset_posY, posZ + offset_posZ);
+										world_gen.registryAccess().registryOrThrow(Registries.CONFIGURED_FEATURE).getHolderOrThrow(FeatureUtils.createKey(feature)).value().place(world_gen, world_gen.getLevel().getChunkSource().getGenerator(), world_gen.getRandom(), pos);
+
+									}
 
 								}
 
+							} else {
+
+								GameUtils.runCommand(level, posX, posY, posZ, read_all);
+
 							}
-
-						} else {
-
-							GameUtils.runCommand(world_gen.getLevel(), posX, posY, posZ, read_all);
 
 						}
 
 					}
 
-				}
+				} buffered_reader.close(); } catch (Exception e) { TanshugetreesMod.LOGGER.error(e.getMessage()); }
 
-			} buffered_reader.close(); } catch (Exception e) { TanshugetreesMod.LOGGER.error(e.getMessage()); }
+			}
 
 		}
 
