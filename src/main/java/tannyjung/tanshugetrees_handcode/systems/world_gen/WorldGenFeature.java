6ea16b82d4911abb8d13bffdcfeb0d6b60fab656
TannyJung <42003724+TannyJungMC@users.noreply.github.com>
2025-05-30 03:54:04 +0700

    Update

--- src/main/java/tannyjung/tanshugetrees_handcode/systems/world_gen/WorldGenFeature.java
+++ src/main/java/tannyjung/tanshugetrees_handcode/systems/world_gen/WorldGenFeature.java
@@ -1,8 +1,14 @@
 package tannyjung.tanshugetrees_handcode.systems.world_gen;
 
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
+import tannyjung.tanshugetrees_handcode.misc.GameUtils;
 
 public class WorldGenFeature extends Feature <NoneFeatureConfiguration> {
 
@@ -15,9 +21,15 @@ public class WorldGenFeature extends Feature <NoneFeatureConfiguration> {
     @Override
     public boolean place (FeaturePlaceContext <NoneFeatureConfiguration> context) {
 
-        TreeLocation.run(context);
-        TreePlacer.run(context);
-        DataFolderCleaner.run(context);
+        LevelAccessor level = context.level();
+        ServerLevel world = context.level().getLevel();
+        ChunkGenerator chunk_generator = context.chunkGenerator();
+        String dimension = GameUtils.getCurrentDimensionID(world).replace(":", "-");
+        ChunkPos chunk_pos = new ChunkPos(context.origin().getX() >> 4, context.origin().getZ() >> 4);
+
+        TreeLocation.run(level, dimension, chunk_pos);
+        TreePlacer.run(level, world, chunk_generator, dimension, chunk_pos);
+        DataFolderCleaner.run(dimension, chunk_pos);
 
         return true;
 
