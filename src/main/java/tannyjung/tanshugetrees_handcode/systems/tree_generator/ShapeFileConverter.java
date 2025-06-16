9a770317a1fcdffd88a6a014b6aef56af8d902f4
TannyJung <42003724+TannyJungMC@users.noreply.github.com>
2025-06-17 02:47:17 +0700

    Start refactoring shape file converter

---
+++ src/main/java/tannyjung/tanshugetrees_handcode/systems/tree_generator/ShapeFileConverter.java
@@ -0,0 +1,13 @@
+package tannyjung.tanshugetrees_handcode.systems.tree_generator;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.LevelAccessor;
+
+public class ShapeFileConverter {
+
+    public static void start (LevelAccessor level, Entity entity) {
+
+
+    }
+
+}
