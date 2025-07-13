28f4ba2a01544955bd6f72afb2236194ba336d26
TannyJung <42003724+TannyJungMC@users.noreply.github.com>
2025-07-13 23:29:15 +0700

    Refactored Preset Fixer

---
+++ src/main/java/tannyjung/tanshugetrees_handcode/systems/tree_generator/PresetFixer.java
@@ -0,0 +1,105 @@
+package tannyjung.tanshugetrees_handcode.systems.tree_generator;
+
+import tannyjung.core.FileManager;
+import tannyjung.core.MiscUtils;
+import tannyjung.tanshugetrees_handcode.Handcode;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PresetFixer {
+
+    public static void start () {
+
+        for (File pack : new File(Handcode.directory_config + "/custom_packs").listFiles()) {
+
+            if (pack.getName().equals(".organized") == false) {
+
+                for (File species : new File(Handcode.directory_config + "/custom_packs/" + pack.getName() + "/presets").listFiles()) {
+
+                    for (File preset : species.listFiles()) {
+
+                        if (preset.isDirectory() == false && preset.getName().endsWith("_settings.txt") == false) {
+
+                            fix(preset);
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+    private static void fix (File file) {
+
+        File template = new File("C:/Users/acer/Desktop/# Tan/Minecraft Modding/TansHugeTrees/run/config/tanshugetrees/custom_packs/TannyJung-Tree-Pack/.dev/preset_template.txt");
+
+        if (template.exists() == true && template.isDirectory() == false) {
+
+            Map<String, String> data = new HashMap<>();
+
+            // Get Preset Data
+            {
+
+                try { BufferedReader buffered_reader = new BufferedReader(new FileReader(file), 65536); String read_all = ""; while ((read_all = buffered_reader.readLine()) != null) {
+
+                    {
+
+                        if (read_all.startsWith(",   ") == true) {
+
+                            data.put(read_all.substring(",   ".length(), read_all.indexOf(":")), read_all);
+
+                        }
+
+                    }
+
+                } buffered_reader.close(); } catch (Exception exception) { MiscUtils.exception(new Exception(), exception); }
+
+            }
+
+            StringBuilder write = new StringBuilder();
+            String name = "";
+
+            // Read Template
+            {
+
+                try { BufferedReader buffered_reader = new BufferedReader(new FileReader(template), 65536); String read_all = ""; while ((read_all = buffered_reader.readLine()) != null) {
+
+                    {
+
+                        if (read_all.startsWith(",") == true) {
+
+                            name = read_all.substring(1, read_all.indexOf(":"));
+
+                            if (data.containsKey(name) == true) {
+
+                                write.append(data.get(name) +  "\n");
+                                continue;
+
+                            }
+
+                        }
+
+                        write.append(read_all + "\n");
+
+                    }
+
+                } buffered_reader.close(); } catch (Exception exception) { MiscUtils.exception(new Exception(), exception); }
+
+            }
+
+            FileManager.writeTXT(file.getPath(), write.toString(), false);
+
+        }
+
+    }
+
+}
