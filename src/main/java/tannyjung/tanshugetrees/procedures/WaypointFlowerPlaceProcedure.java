648a49107eeddf69746093e20df5365581635973
TannyJung <42003724+TannyJungMC@users.noreply.github.com>
2025-07-15 01:29:21 +0700

    Completely remove file manager plugin from the mod

--- src/main/java/tannyjung/tanshugetrees/procedures/WaypointFlowerPlaceProcedure.java
+++ src/main/java/tannyjung/tanshugetrees/procedures/WaypointFlowerPlaceProcedure.java
@@ -1,17 +1,8 @@
 package tannyjung.tanshugetrees.procedures;
 
-import net.minecraftforge.fml.loading.FMLPaths;
-
-import net.minecraft.world.phys.Vec3;
-import net.minecraft.world.phys.Vec2;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.network.chat.Component;
 import net.minecraft.core.BlockPos;
-import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.commands.CommandSource;
 
 import java.io.File;
 
@@ -22,32 +13,10 @@ public class WaypointFlowerPlaceProcedure {
 		String merge_text = "";
 		String file_name = "";
 		String theme = "";
-		file = new File((FMLPaths.GAMEDIR.get().toString() + "/config/tanshugetrees/custom_packs/TannyJung-Tree-Pack/functions"), File.separator + "sapling_trader.txt");
-		if (file.exists() == true) {
-			if (true) {
-				if (world instanceof ServerLevel _level)
-					_level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
-							"playsound minecraft:entity.illusioner.cast_spell ambient @a[distance=..100] ~0.5 ~0.5 ~0.5 2 0 0.025");
-				if (world instanceof ServerLevel _level)
-					_level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
-							"particle minecraft:campfire_signal_smoke ~0.5 ~0.5 ~0.5 0.5 0.5 0.5 0.01 20 force");
-				if (world instanceof ServerLevel _level)
-					_level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
-							"particle minecraft:flash ~0.5 ~0.5 ~0.5 0 0 0 0 1 force");
-			}
-			if (world instanceof ServerLevel _level)
-				_level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3((x + 0.5), y, (z + 0.5)), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
-						"TANSHUGETREES command txt_function sapling_trader config/tanshugetrees/custom_packs/TannyJung-Tree-Pack/functions");
-			world.setBlock(BlockPos.containing(x, y, z), Blocks.AIR.defaultBlockState(), 3);
-		} else {
-			if (world instanceof ServerLevel _level)
-				_level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
-						"tellraw @a [\"\",{\"text\":\"THT : Can't find preset for this sapling in config folder\",\"color\":\"red\"}]");
-			{
-				BlockPos _pos = BlockPos.containing(x, y, z);
-				Block.dropResources(world.getBlockState(_pos), world, BlockPos.containing(x, y, z), null);
-				world.destroyBlock(_pos, false);
-			}
+		{
+			BlockPos _pos = BlockPos.containing(x, y, z);
+			Block.dropResources(world.getBlockState(_pos), world, BlockPos.containing(x, y, z), null);
+			world.destroyBlock(_pos, false);
 		}
 	}
 }
