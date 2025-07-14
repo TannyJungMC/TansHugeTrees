f4df654f1f93789d51fb686e5b9d4feecf251178
TannyJung <42003724+TannyJungMC@users.noreply.github.com>
2025-07-15 01:30:30 +0700

    Move preset fixer to command

---
+++ src/main/java/tannyjung/tanshugetrees/procedures/COMMANDPresetFixerRunProcedure.java
@@ -0,0 +1,9 @@
+package tannyjung.tanshugetrees.procedures;
+
+import tannyjung.tanshugetrees_handcode.systems.tree_generator.PresetFixer;
+
+public class COMMANDPresetFixerRunProcedure {
+	public static void execute() {
+		PresetFixer.start();
+	}
+}
