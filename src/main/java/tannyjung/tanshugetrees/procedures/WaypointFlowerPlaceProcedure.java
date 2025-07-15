e9b73ec12820341acff330de45c20c617461cfb7
TannyJung <42003724+TannyJungMC@users.noreply.github.com>
2025-07-15 19:01:46 +0700

    Update

--- src/main/java/tannyjung/tanshugetrees/procedures/WaypointFlowerPlaceProcedure.java
+++ src/main/java/tannyjung/tanshugetrees/procedures/WaypointFlowerPlaceProcedure.java
@@ -4,11 +4,8 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.core.BlockPos;
 
-import java.io.File;
-
 public class WaypointFlowerPlaceProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
-		File file = new File("");
 		double merge_text_pos = 0;
 		String merge_text = "";
 		String file_name = "";
