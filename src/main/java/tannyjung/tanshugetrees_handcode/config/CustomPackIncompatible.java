9c7951dd81d9958a9b701b7068f96ff8f8a889f2
TannyJung <42003724+TannyJungMC@users.noreply.github.com>
2025-07-21 02:09:39 +0700

    Some fixes in pack incompatibility detection

--- src/main/java/tannyjung/tanshugetrees_handcode/config/CustomPackIncompatible.java
+++ src/main/java/tannyjung/tanshugetrees_handcode/config/CustomPackIncompatible.java
@@ -18,21 +18,17 @@ public class CustomPackIncompatible {
 
     public static void scanMain (LevelAccessor level_accessor) {
 
-        String path = "";
-
         for (File pack : new File(Handcode.directory_config + "/custom_packs").listFiles()) {
 
             if (pack.getName().equals(".organized") == false) {
 
-                path = pack.toPath().toString().replace("[INCOMPATIBLE] ", "");
-
-                if (testVersion(level_accessor, new File(path + "/version.txt")) == false) {
+                if (testVersion(level_accessor, new File(pack + "/version.txt")) == false) {
 
                     break;
 
                 }
 
-                if (testDependencies(level_accessor, new File(path + "/dependencies.txt")) == false) {
+                if (testDependencies(level_accessor, new File(pack + "/dependencies.txt")) == false) {
 
                     break;
 
@@ -101,6 +97,7 @@ public class CustomPackIncompatible {
 
         boolean pass = true;
         String message = "";
+        String pack_name = file.getParentFile().getName().replace("[INCOMPATIBLE] ", "");
 
         if (file.exists() == true && file.isDirectory() == false) {
 
@@ -115,7 +112,7 @@ public class CustomPackIncompatible {
                             if (Double.parseDouble(read_all.replace("data_structure_version = ", "")) != Handcode.data_structure_version_pack) {
 
                                 pass = false;
-                                message = "Detected incompatible pack. Caused by unsupported mod version. [ " + file.getParentFile().getName().replace("[INCOMPATIBLE] ", "") + " ]";
+                                message = "Detected incompatible pack. Caused by unsupported mod version. [ " + pack_name + " ]";
                                 break;
 
                             }
@@ -131,7 +128,7 @@ public class CustomPackIncompatible {
         } else {
 
             pass = false;
-            message = "Detected incompatible pack. Caused by no version file. [ " + file.getParentFile().getName().replace("[INCOMPATIBLE] ", "") + " ]";
+            message = "Detected incompatible pack. Caused by no version file. [ " + pack_name + " ]";
 
         }
 
@@ -154,6 +151,7 @@ public class CustomPackIncompatible {
 
         boolean pass = true;
         String message = "";
+        String name_pack = file.getParentFile().getName().replace("[INCOMPATIBLE] ", "");
 
         if (file.exists() == true && file.isDirectory() == false) {
 
@@ -178,7 +176,7 @@ public class CustomPackIncompatible {
                                         if (new File(Handcode.directory_config + "/custom_packs/" + test).exists() == false) {
 
                                             pass = false;
-                                            message = "Detected incompatible pack. Caused by required pack not found. [ " + file.getParentFile().getName() + " > " + test + " ]";
+                                            message = "Detected incompatible pack. Caused by required pack not found. [ " + name_pack + " > " + test + " ]";
                                             break;
 
                                         }
@@ -202,7 +200,7 @@ public class CustomPackIncompatible {
                                         if (ModList.get().isLoaded(test) == false) {
 
                                             pass = false;
-                                            message = "Detected incompatible pack. Caused by required mod not found. [ " + file.getParentFile().getName() + " > " + test + " ]";
+                                            message = "Detected incompatible pack. Caused by required mod not found. [ " + name_pack + " > " + test + " ]";
                                             break;
 
                                         }
@@ -243,6 +241,9 @@ public class CustomPackIncompatible {
         boolean pass = true;
         String message = "";
         String tree_settings = "";
+        String name_pack = file.getParentFile().getParentFile().getName().replace("[INCOMPATIBLE] ", "");
+        String name_theme = file.getParentFile().getName();
+        String name_tree = file.getName().replace("[INCOMPATIBLE] ", "");
 
         // Read "World Gen" File
         {
@@ -264,7 +265,7 @@ public class CustomPackIncompatible {
 
         }
 
-        File file_settings = new File(Handcode.directory_config + "/custom_packs/.organized/presets/" + tree_settings.replace("[INCOMPATIBLE] ", ""));
+        File file_settings = new File(Handcode.directory_config + "/custom_packs/.organized/presets/" + tree_settings);
 
         if (file_settings.exists() == true && file_settings.isDirectory() == false) {
 
@@ -284,7 +285,7 @@ public class CustomPackIncompatible {
                                 if (GameUtils.block.fromText(id).getBlock() == Blocks.AIR) {
 
                                     pass = false;
-                                    message = "Detected incompatible tree. Caused by unknown block ID. [ " + file.getParentFile().getName().replace("[INCOMPATIBLE] ", "") + " > " + file.getName().replace("[INCOMPATIBLE] ", "") + " ]";
+                                    message = "Detected incompatible tree. Caused by unknown block ID. [ " + name_pack + " > " + name_tree + " > " + id + " ]";
                                     break;
 
                                 }
@@ -302,17 +303,13 @@ public class CustomPackIncompatible {
         } else {
 
             pass = false;
-            message = "Detected incompatible tree. Caused by tree settings not found. [ " + file.getParentFile().getParentFile().getName() + " > " + file.getParentFile().getName().replace("[INCOMPATIBLE] ", "") + " > " + file.getName().replace("[INCOMPATIBLE] ", "") + " ]";
+            message = "Detected incompatible tree. Caused by tree settings not found. [ " + name_pack + " > " + name_theme + " > " + name_tree + " ]";
 
         }
 
         if (message.equals("") == false) {
 
-            if (level_accessor instanceof ServerLevel level_server) {
-
-                TanshugetreesMod.LOGGER.error(message);
-
-            }
+            TanshugetreesMod.LOGGER.error(message);
 
         }
 
