2f3deecb50827d731d3449b3a1f8d4cb59447f71
TannyJung <42003724+TannyJungMC@users.noreply.github.com>
2025-07-23 00:00:56 +0700

    Update

--- src/main/java/tannyjung/tanshugetrees/procedures/COMMANDPackCheckUpdateProcedure.java
+++ src/main/java/tannyjung/tanshugetrees/procedures/COMMANDPackCheckUpdateProcedure.java
@@ -16,6 +16,6 @@ public class COMMANDPackCheckUpdateProcedure {
 			if (world instanceof ServerLevel _level)
 				_level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(), "");
 		}
-		PackCheckUpdate.start(world);
+		PackCheckUpdate.start(world, true);
 	}
 }
