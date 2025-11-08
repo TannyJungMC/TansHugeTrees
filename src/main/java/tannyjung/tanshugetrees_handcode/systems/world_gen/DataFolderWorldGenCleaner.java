0f3a038d8861886b3157373536ffdc92096f2143
TannyJung <42003724+TannyJungMC@users.noreply.github.com>
2025-11-09 02:42:34 +0700

    Region chunk left test now use BIN instead of TXT

--- src/main/java/tannyjung/tanshugetrees_handcode/systems/world_gen/DataFolderWorldGenCleaner.java
+++ src/main/java/tannyjung/tanshugetrees_handcode/systems/world_gen/DataFolderWorldGenCleaner.java
@@ -2,61 +2,33 @@ package tannyjung.tanshugetrees_handcode.systems.world_gen;
 
 import net.minecraft.world.level.ChunkPos;
 import tannyjung.core.FileManager;
-import tannyjung.core.GameUtils;
 import tannyjung.tanshugetrees_handcode.Handcode;
-import tannyjung.core.OutsideUtils;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DataFolderWorldGenCleaner {
 
     public static void start (String dimension, ChunkPos chunk_pos) {
 
         String region = (chunk_pos.x >> 5) + "," + (chunk_pos.z >> 5);
-        String node = GameUtils.outside.quardtreeChunkToNode(chunk_pos.x, chunk_pos.z);
-        String file_path = dimension + "/" + region + "/" + node + ".txt";
+        String file_path = dimension + "/" + region;
+        File file_region = new File(Handcode.path_world_data + "/world_gen/regions/" + file_path + ".bin");
+        File file_place = new File(Handcode.path_world_data + "/world_gen/place/" + file_path + ".txt");
+        File file_detailed_detection = new File(Handcode.path_world_data + "/world_gen/detailed_detection/" + file_path + ".txt");
 
-        File file_region = new File(Handcode.path_world_data + "/world_gen/regions/" + file_path);
-        FileManager.writeTXT(file_region.toPath().toString(), "A", true);
-        boolean full_node = false;
+        List<String> list = new ArrayList<>();
+        list.add("b0");
+        FileManager.writeBIN(file_region.getPath(), list, true);
 
-        // Read to get text length
-        {
-
-            try { BufferedReader buffered_reader = new BufferedReader(new FileReader(file_region), 65536); String read_all = ""; while ((read_all = buffered_reader.readLine()) != null) {
-
-                {
-
-                    if (read_all.length() >= (32 >> 2) * (32 >> 2)) {
-
-                        full_node = true;
-
-                    }
-
-                }
-
-            } buffered_reader.close(); } catch (Exception exception) { OutsideUtils.exception(new Exception(), exception); }
-
-        }
-
-        if (full_node == true) {
-
-            File file_place = new File(Handcode.path_world_data + "/world_gen/place/" + file_path);
-            File file_detailed_detection = new File(Handcode.path_world_data + "/world_gen/detailed_detection/" + file_path);
+        if (FileManager.readBIN(file_region.getPath()).capacity() >= 1024) {
 
             file_region.delete();
             file_place.delete();
             file_detailed_detection.delete();
 
-            // If finish placing all chunks in region
-            if (file_place.getParentFile().listFiles() == null || file_place.getParentFile().listFiles().length == 0) {
-
-                file_place.getParentFile().delete();
-                file_detailed_detection.getParentFile().delete();
-
-            }
+            System.out.println("Deleted Region -> " + region);
 
         }
 
