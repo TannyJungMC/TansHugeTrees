0c8dea3ce4d0319c6e99d9012f6e02268fb81169
TannyJung <42003724+TannyJungMC@users.noreply.github.com>
2025-11-12 00:48:17 +0700

    Fix data folder cleaner not work properly

--- src/main/java/tannyjung/tanshugetrees_handcode/systems/world_gen/DataFolderWorldGenCleaner.java
+++ src/main/java/tannyjung/tanshugetrees_handcode/systems/world_gen/DataFolderWorldGenCleaner.java
@@ -13,22 +13,18 @@ public class DataFolderWorldGenCleaner {
     public static void start (String dimension, ChunkPos chunk_pos) {
 
         String region = (chunk_pos.x >> 5) + "," + (chunk_pos.z >> 5);
-        String file_path = dimension + "/" + region;
-        File file_region = new File(Handcode.path_world_data + "/world_gen/regions/" + file_path + ".bin");
-        File file_place = new File(Handcode.path_world_data + "/world_gen/place/" + file_path + ".txt");
-        File file_detailed_detection = new File(Handcode.path_world_data + "/world_gen/detailed_detection/" + file_path + ".txt");
+        String file_path = dimension + "/" + region + ".bin";
 
+        File file_region = new File(Handcode.path_world_data + "/world_gen/#regions/" + file_path);
         List<String> list = new ArrayList<>();
         list.add("b0");
         FileManager.writeBIN(file_region.getPath(), list, true);
 
         if (FileManager.readBIN(file_region.getPath()).capacity() >= 1024) {
 
-            file_region.delete();
-            file_place.delete();
-            file_detailed_detection.delete();
-
-            System.out.println("Deleted Region -> " + region);
+            FileManager.writeBIN(file_region.getPath(), new ArrayList<>(), false);
+            new File(Handcode.path_world_data + "/world_gen/place/" + file_path).delete();
+            new File(Handcode.path_world_data + "/world_gen/detailed_detection/" + file_path).delete();
 
         }
 
