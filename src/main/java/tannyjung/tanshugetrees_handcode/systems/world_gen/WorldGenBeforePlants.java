3cd232edb9ac7ae303fa70e7d120f82ca4bf4794
TannyJung <42003724+TannyJungMC@users.noreply.github.com>
2025-11-14 06:11:14 +0700

    Fix multi-thread incompatible. Lock region pre-location to only run one per time.

--- src/main/java/tannyjung/tanshugetrees_handcode/systems/world_gen/WorldGenBeforePlants.java
+++ src/main/java/tannyjung/tanshugetrees_handcode/systems/world_gen/WorldGenBeforePlants.java
@@ -17,6 +17,8 @@ public class WorldGenBeforePlants extends Feature <NoneFeatureConfiguration> {
 
     }
 
+    private final Object lock = new Object();
+
     @Override
     public boolean place (FeaturePlaceContext <NoneFeatureConfiguration> context) {
 
@@ -26,10 +28,15 @@ public class WorldGenBeforePlants extends Feature <NoneFeatureConfiguration> {
         String dimension = Utils.misc.getCurrentDimensionID(level_server).replace(":", "-");
         ChunkPos chunk_pos = new ChunkPos(context.origin().getX() >> 4, context.origin().getZ() >> 4);
 
-        TreeLocation.start(level_accessor, dimension, new ChunkPos(chunk_pos.x + 4, chunk_pos.z + 4));
-        TreeLocation.start(level_accessor, dimension, new ChunkPos(chunk_pos.x + 4, chunk_pos.z - 4));
-        TreeLocation.start(level_accessor, dimension, new ChunkPos(chunk_pos.x - 4, chunk_pos.z + 4));
-        TreeLocation.start(level_accessor, dimension, new ChunkPos(chunk_pos.x - 4, chunk_pos.z - 4));
+        synchronized (lock) {
+
+            TreeLocation.start(level_accessor, dimension, new ChunkPos(chunk_pos.x + 4, chunk_pos.z + 4));
+            TreeLocation.start(level_accessor, dimension, new ChunkPos(chunk_pos.x + 4, chunk_pos.z - 4));
+            TreeLocation.start(level_accessor, dimension, new ChunkPos(chunk_pos.x - 4, chunk_pos.z + 4));
+            TreeLocation.start(level_accessor, dimension, new ChunkPos(chunk_pos.x - 4, chunk_pos.z - 4));
+
+        }
+
         TreePlacer.start(level_accessor, level_server, chunk_generator, dimension, chunk_pos);
 
         return true;
