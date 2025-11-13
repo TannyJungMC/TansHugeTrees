5ae23c9c749e9e361f55d814b0ceda3bfba17cc3
TannyJung <42003724+TannyJungMC@users.noreply.github.com>
2025-11-14 06:10:20 +0700

    Make all commands run in server thread. Fix summon entity make game tick freeze in multi-thread.

--- src/main/java/tannyjung/core/Utils.java
+++ src/main/java/tannyjung/core/Utils.java
@@ -449,7 +449,11 @@ public class Utils {
 
 		public static void run (ServerLevel level_server, double posX, double posY, double posZ, String command) {
 
-			level_server.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(posX, posY, posZ), Vec2.ZERO, level_server, 4, "", Component.literal(""), level_server.getServer(), null).withSuppressedOutput(), command);
+            level_server.getServer().execute(() -> {
+
+                level_server.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(posX, posY, posZ), Vec2.ZERO, level_server, 4, "", Component.literal(""), level_server.getServer(), null).withSuppressedOutput(), command);
+
+            });
 
 		}
 
@@ -457,7 +461,11 @@ public class Utils {
 
 			if (entity.level() instanceof ServerLevel level_server) {
 
-				level_server.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, entity.position(), entity.getRotationVector(), level_server, 4, entity.getName().getString(), entity.getDisplayName(), level_server.getServer(), entity), command);
+                level_server.getServer().execute(() -> {
+
+                    level_server.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, entity.position(), entity.getRotationVector(), level_server, 4, entity.getName().getString(), entity.getDisplayName(), level_server.getServer(), entity), command);
+
+                });
 
 			}
 
