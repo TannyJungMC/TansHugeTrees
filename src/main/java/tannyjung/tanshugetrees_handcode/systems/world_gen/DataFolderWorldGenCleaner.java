6b66afeacbc7cbc26982a74e15754776d23875a6
TannyJung <42003724+TannyJungMC@users.noreply.github.com>
2025-11-24 05:00:20 +0700

    Fix region file not change size when all chunks are placed (1,024B -> 1B)

--- src/main/java/tannyjung/tanshugetrees_handcode/systems/world_gen/DataFolderWorldGenCleaner.java
+++ src/main/java/tannyjung/tanshugetrees_handcode/systems/world_gen/DataFolderWorldGenCleaner.java
@@ -12,19 +12,21 @@ public class DataFolderWorldGenCleaner {
 
     public static void start (String dimension, ChunkPos chunk_pos) {
 
-        String region = (chunk_pos.x >> 5) + "," + (chunk_pos.z >> 5);
-        String file_path = dimension + "/" + region + ".bin";
-
+        String file_path = dimension + "/" + (chunk_pos.x >> 5) + "," + (chunk_pos.z >> 5) + ".bin";
         File file_region = new File(Handcode.path_world_data + "/world_gen/#regions/" + file_path);
-        List<String> list = new ArrayList<>();
-        list.add("b0");
-        FileManager.writeBIN(file_region.getPath(), list, true);
+
+        List<String> add = new ArrayList<>();
+        add.add("b0");
+        FileManager.writeBIN(file_region.getPath(), add, true);
 
         if (FileManager.readBIN(file_region.getPath()).capacity() >= 1024) {
 
-            FileManager.writeBIN(file_region.getPath(), new ArrayList<>(), false);
             new File(Handcode.path_world_data + "/world_gen/place/" + file_path).delete();
             new File(Handcode.path_world_data + "/world_gen/detailed_detection/" + file_path).delete();
+
+            List<String> add_end = new ArrayList<>();
+            add_end.add("b1");
+            FileManager.writeBIN(file_region.getPath(), add_end, false);
 
         }
 
