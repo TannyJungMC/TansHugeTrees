53bdfe68088eb1424a458314b70879ca1ad567a8
TannyJung <42003724+TannyJungMC@users.noreply.github.com>
2026-03-29 09:39:08 +0700

    Some fixes in dynamic config + Now with default values

--- src/main/java/tannyjung/tanshugetrees_core/outside/ConfigDynamic.java
+++ src/main/java/tannyjung/tanshugetrees_core/outside/ConfigDynamic.java
@@ -6,96 +6,82 @@ import tannyjung.tanshugetrees_core.Core;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ConfigDynamic {
 
-    public static void reorganize (String name, String scan_at, String description) {
+    public static void reorganize (String name, String scan_at, String options) {
 
-        File file = new File(Core.path_config + "/config_" + name + ".txt");
-        File file_temp = new File(Core.path_config + "/config_" + name + "_temp.txt");
+        LinkedHashMap<String, String> description = new LinkedHashMap<>();
 
-        // Create Temp
+        // Get Data
         {
 
-            if (file.exists() == true) {
+            List<String> data = new ArrayList<>();
+            String[] split = new String[0];
 
-                try {
+            for (String read_all : options.split("\n")) {
 
-                    Files.copy(file.toPath(), file_temp.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                if (read_all.startsWith("# ") == false) {
 
-                } catch (Exception exception) {
+                    data.add(read_all);
+                    split = read_all.split(" = ");
 
-                    OutsideUtils.exception(new Exception(), exception, "");
+                } else {
+
+                    description.put(split[0], read_all.substring("# ".length()));
 
                 }
 
             }
 
+            CacheManager.Data.setMapTextListText("config_dynamic_default", name, data);
+
         }
 
-        create(name, scan_at, description);
+        StringBuilder write = new StringBuilder();
+        write.append("Important Notes");
+        write.append("\n");
+        write.append("\n");
+        write.append("- To apply this config and repair missing values, run this command [ /").append(Core.mod_id_big).append(" restart ] or restart the world.");
+        write.append("\n");
+        write.append("- Very important! You must lock the settings you have edited to prevent them from resetting. Do it by change [] at font of ID to [LOCK]. This is for keeping config values you haven't changed to always up to date.");
+        write.append("\n");
+        write.append("\n");
+        write.append("Config Description");
+        write.append("\n");
+        write.append("\n");
 
-        // Delete Temp
+        // Write Description
         {
 
-            if (file_temp.exists() == true) {
+            for (Map.Entry<String, String> entry : description.entrySet()) {
 
-                try {
-
-                    Files.delete(file_temp.toPath());
-
-                } catch (Exception exception) {
-
-                    OutsideUtils.exception(new Exception(), exception, "");
-
-                }
+                write.append("- ").append(entry.getKey()).append(" : ").append(description.get(entry.getKey()));
+                write.append("\n");
 
             }
 
         }
 
-    }
-
-    private static void create (String name, String scan_at, String description) {
-
+        write.append("\n");
         File file_organized = new File(Core.path_config + "/dev/temporary/" + scan_at);
 
-        if (file_organized.exists() == true && file_organized.isDirectory() == true) {
+        if (file_organized.exists() == false) {
 
-            File file = new File(Core.path_config + "/config_" + name + ".txt");
+            write.append("----------------------------------------------------------------------------------------------------");
+            write.append("\n");
+            write.append("\n");
+            write.append("Nothing to show here. Try create a world and join in first.");
+            write.append("\n");
+            write.append("\n");
 
-            // Re-Create The File
-            {
-
-                StringBuilder write = new StringBuilder();
-
-                {
-
-                    write.append("Important Notes");
-                    write.append("\n");
-                    write.append("\n");
-                    write.append("- To apply this config and repair missing values, run this command [ /").append(Core.mod_id_big).append(" restart ] or restart the world.");
-                    write.append("\n");
-                    write.append("- Very important! You must lock the trees you have edited to prevent them from resetting. Do it by change \"[]\" at font of ID to \"[LOCK]\". This is for keeping config values you haven't changed to always up to date.");
-                    write.append("\n");
-                    write.append("\n");
-                    write.append("Config Description");
-                    write.append("\n");
-                    write.append("\n");
-                    write.append(description);
-                    write.append("\n");
-
-                }
-
-                FileManager.writeTXT(file.toPath().toString(), write.toString(), false);
-
-            }
+        } else {
 
             // Scan Packs
             {
+
+                Map<String, Map<String, String>> temp = getData(name);
 
                 try {
 
@@ -103,7 +89,7 @@ public class ConfigDynamic {
 
                         if (source.toFile().isDirectory() == false) {
 
-                            write(name, scan_at, source);
+                            write.append(write(name, scan_at, temp, source));
 
                         }
 
@@ -117,180 +103,135 @@ public class ConfigDynamic {
 
             }
 
-            FileManager.writeTXT(file.toPath().toString(), "----------------------------------------------------------------------------------------------------", true);
-
         }
+
+        write.append("----------------------------------------------------------------------------------------------------");
+        FileManager.writeTXT(Core.path_config + "/config_" + name + ".txt", write.toString(), false);
 
     }
 
-    private static void write (String name, String scan_at, Path source) {
+    private static String write (String name, String scan_at, Map<String, Map<String, String>> temp, Path source) {
 
-        String path = Path.of(Core.path_config + "/dev/temporary/" + scan_at).relativize(source).toString().replace("\\", " > ").replace(".txt", "");;
+        String id = Path.of(Core.path_config + "/dev/temporary/" + scan_at).relativize(source).toString().replace("\\", "/").replace(".txt", "");;
         boolean incompatible = false;
 
-        if (path.contains("[INCOMPATIBLE] ") == true) {
+        if (id.contains("[INCOMPATIBLE] ") == true) {
 
-            path = path.replace("[INCOMPATIBLE] ", "");
+            id = id.replace("[INCOMPATIBLE] ", "");
             incompatible = true;
 
         }
 
-        boolean replace = true;
+        boolean lock = false;
 
-        // Test is it locked
-        {
+        if (temp.containsKey(id) == true) {
 
-            for (String read_all : FileManager.readTXT(Core.path_config + "/config_" + name + "_temp.txt")) {
-
-                {
-
-                    if (read_all.startsWith("[") == true && read_all.endsWith("] " + path) == true) {
-
-                        if (read_all.replace("[INCOMPATIBLE]", "").startsWith("[LOCK]") == true) {
-
-                            replace = false;
-
-                        }
-
-                        break;
-
-                    }
-
-                }
-
-            }
+            lock = temp.get(id).get("lock").equals("true") == true;
 
         }
 
         StringBuilder write = new StringBuilder();
 
-        // Write
+        // Name
         {
 
-            // Name
-            {
+            write.append("----------------------------------------------------------------------------------------------------");
+            write.append("\n");
 
-                write.append("----------------------------------------------------------------------------------------------------");
-                write.append("\n");
+            if (incompatible == true) {
 
-                if (incompatible == true) {
-
-                    write.append("[INCOMPATIBLE] ");
-
-                }
-
-                if (replace == false) {
-
-                    write.append("[LOCK] ");
-
-                } else {
-
-                    write.append("[] ");
-
-                }
-
-                write.append(path);
-                write.append("\n");
-                write.append("----------------------------------------------------------------------------------------------------");
-                write.append("\n");
+                write.append("[INCOMPATIBLE] ");
 
             }
 
-            String option = "";
+            if (lock == true) {
 
-            for (String read_all : FileManager.readTXT(source.toString())) {
+                write.append("[LOCK] ");
 
-                {
+            } else {
 
-                    if (read_all.isEmpty() == false) {
-
-                        if (replace == true) {
-
-                            write.append(read_all);
-
-                        } else {
-
-                            // Get Old Value
-                            {
-
-                                File file_temp = new File(Core.path_config + "/config_" + name + "_temp.txt");
-                                boolean thisID = false;
-                                option = read_all.substring(0, read_all.indexOf(" = "));
-
-                                for (String read_all_temp : FileManager.readTXT(file_temp.getPath())) {
-
-                                    {
-
-                                        if (thisID == false) {
-
-                                            if (read_all_temp.startsWith("[") == true) {
-
-                                                if (read_all_temp.endsWith(path) == true) {
-
-                                                    thisID = true;
-
-                                                }
-
-                                            }
-
-                                        } else {
-
-                                            if (read_all_temp.startsWith(option) == true) {
-
-                                                write.append(read_all_temp);
-                                                break;
-
-                                            } else if (read_all_temp.startsWith("[") == true) {
-
-                                                // If not found this option in temp file
-                                                write.append(read_all_temp);
-                                                break;
-
-                                            }
-
-                                        }
-
-                                    }
-
-                                }
-
-                            }
-
-                        }
-
-                        write.append("\n");
-
-                    }
-
-                }
+                write.append("[] ");
 
             }
+
+            write.append(id.replace("/", " > "));
+            write.append("\n");
+            write.append("----------------------------------------------------------------------------------------------------");
+            write.append("\n");
 
         }
 
-        FileManager.writeTXT(Core.path_config + "/config_" + name + ".txt", write.toString(), true);
+        Map<String, String> options = FileManager.convertFileToDataMap(source.toString());
+        String[] split = new String[0];
 
-    }
+        for (String scan : CacheManager.Data.getMapTextListText("config_dynamic_default").get(name)) {
 
-    public static Map<String, Map<String, String>> getData (String name, String default_values) {
+            split = scan.split(" = ");
 
-        if (CacheManager.Data.existTextText(name) == false) {
+            if (options.containsKey(split[0]) == true) {
 
-            Map<String, Map<String, String>> data = new HashMap<>();
-            Map<String, String> settings = new HashMap<>();
-            String[] value = new String[0];
-            String id = "";
-            boolean skip = true;
-            boolean after_this = false;
-            String[] split = new String[0];
+                if (temp.containsKey(id) == true && temp.get(id).containsKey(split[0]) == true) {
 
-            if (CacheManager.Data.existTextList("config_dynamic", name) == false) {
+                    if (lock == true) {
 
-                CacheManager.Data.setTextList("config_dynamic", name, FileManager.readTXT(Core.path_config + "/config_" + name + ".txt"));
+                        write.append(split[0]).append(" = ").append(temp.get(id).get(split[0]));
+
+                    } else {
+
+                        write.append(split[0]).append(" = ").append(options.get(split[0]));
+
+                    }
+
+                    write.append("\n");
+                    continue;
+
+                }
 
             }
 
-            for (String read_all : CacheManager.Data.getTextList("config_dynamic", name)) {
+            write.append(split[0]).append(" = ").append(split[1]);
+            write.append("\n");
+
+        }
+
+        return write.toString();
+
+    }
+
+    public static Map<String, Map<String, String>> getData (String name) {
+
+        if (CacheManager.Data.existMapTextLogic("create_config_" + name, "") == false) {
+
+            CacheManager.Data.setMapTextLogic("create_config_" + name, "", true);
+
+            Map<String, String> default_values = new HashMap<>();
+
+            // Get Default Values
+            {
+
+                String[] split = new String[0];
+
+                for (String scan : CacheManager.Data.getMapTextListText("config_dynamic_default").get(name)) {
+
+                    split = scan.split(" = ");
+                    default_values.put(split[0], split[1]);
+
+                }
+
+            }
+
+            Map<String, Map<String, String>> data = new HashMap<>();
+            String[] value = new String[0];
+            String id = "";
+            boolean skip = true;
+
+            if (CacheManager.Data.existMapTextListText("config_dynamic", name) == false) {
+
+                CacheManager.Data.setMapTextListText("config_dynamic", name, FileManager.readTXT(Core.path_config + "/config_" + name + ".txt"));
+
+            }
+
+            for (String read_all : CacheManager.Data.getMapTextListText("config_dynamic").get(name)) {
 
                 {
 
@@ -298,50 +239,40 @@ public class ConfigDynamic {
 
                         if (read_all.startsWith("[") == true) {
 
-                            if (read_all.startsWith("[INCOMPATIBLE]") == true) {
+                            // First Data
+                            {
 
-                                skip = true;
+                                if (read_all.startsWith("[INCOMPATIBLE] ") == true) {
 
-                            } else {
+                                    skip = true;
+                                    read_all = read_all.substring("[INCOMPATIBLE] ".length());
 
-                                skip = false;
+                                } else {
+
+                                    skip = false;
+
+                                }
+
                                 id = read_all.substring(read_all.indexOf("]") + 2).replace(" > ", "/");
+                                data.computeIfAbsent(id, test -> new HashMap<>()).put("lock", String.valueOf(read_all.startsWith("[LOCK] ") == true));
 
                             }
 
-                        } else {
+                        } else if (read_all.contains(" = ") == true) {
 
                             if (skip == false) {
 
-                                if (read_all.contains(" = ") == true) {
+                                value = read_all.split(" = ");
+                                data.computeIfAbsent(id, test -> new HashMap<>()).put(value[0], value[1]);
 
-                                    after_this = true;
-                                    value = read_all.split(" = ");
-                                    settings.put(value[0], value[1]);
+                            } else {
 
-                                } else if (after_this == true) {
+                                value = read_all.split(" = ");
 
-                                    after_this = false;
+                                if (default_values.containsKey(value[0]) == true) {
 
-                                    // Test Non-Exist Options
-                                    {
-
-                                        for (String option : default_values.split(" / ")) {
-
-                                            split = option.split(" -> ");
-
-                                            if (settings.containsKey(split[0]) == false) {
-
-                                                settings.put(split[0], split[1]);
-
-                                            }
-
-                                        }
-
-                                    }
-
-                                    data.put(id, new HashMap<>(settings));
-                                    settings.clear();
+                                    value[1] = default_values.get(value[0]);
+                                    data.computeIfAbsent(id, test -> new HashMap<>()).put(value[0], value[1]);
 
                                 }
 
@@ -355,11 +286,15 @@ public class ConfigDynamic {
 
             }
 
-            CacheManager.Data.setTextText(name, data);
+            for (Map.Entry<String, Map<String, String>> entry : data.entrySet()) {
+
+                CacheManager.Data.setMapTextMapTextText("config_" + name, entry.getKey(), entry.getValue());
+
+            }
 
         }
 
-        return CacheManager.Data.getTextText(name);
+        return CacheManager.Data.getMapTextMapTextText("config_" + name);
 
     }
 
